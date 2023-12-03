f2cbe9762e6e23ddf9ccad06b73f13426d4202db
iulia0223 <yuliia.tsiuvanyk.shi.2022@lpnu.ua>
2023-12-03 22:02:14 +0300

    New final files

--- playlist_service/playlist/tests.py
+++ playlist_service/playlist/tests.py
@@ -18,3 +18,10 @@ class PlaylistTests(TestCase):
         response = self.client.get(reverse('playlist-detail', args=[str(self.playlist.id)]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Playlist')
+
+    def test_invalid_playlist_creation(self):
+        # Викликаємо API для створення плейлисту із некоректними даними
+        response = self.client.post(reverse('playlist-list'), data={'invalid_field': 'Invalid Value'})
+
+        # Перевіряємо, чи повертається статус код 400 (Bad Request)
+        self.assertEqual(response.status_code, 400)
